f6eade910a69572b254f0d23c95d1d8bd3170d27
Ivan8or <ivanthegreat825@gmail.com>
2022-01-16 16:26:40 -0500

    updated hash function to be sha512 over sha256

--- src/main/java/online/umbcraft/hash/HashHelper.java
+++ src/main/java/online/umbcraft/hash/HashHelper.java
@@ -7,11 +7,11 @@ import java.util.Base64;
 
 public class HashHelper {
 
-    private static MessageDigest sha256digest;
+    private static MessageDigest sha512digest;
 
     static {
         try {
-            sha256digest = MessageDigest.getInstance("SHA-256");
+            sha512digest = MessageDigest.getInstance("SHA-512");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -20,7 +20,7 @@ public class HashHelper {
     // gets the base 64 encoded hash of a string of text - only works with UTF-8 characters
     public static String getHash(String plaintext) {
 
-        byte[] byteHash = sha256digest.digest(plaintext.getBytes(StandardCharsets.UTF_8));
+        byte[] byteHash = sha512digest.digest(plaintext.getBytes(StandardCharsets.UTF_8));
         String base64hash = Base64.getEncoder().encodeToString(byteHash);
         return base64hash;
 
