8a78bb349be57197c66e842ad264c5b9b0cdd1e0
Ivan8or <ivanthegreat825@gmail.com>
2022-01-17 00:00:56 -0500

    add new algorithms for keyscraping

--- src/main/java/online/umbcraft/web/SetKeyScraper.java
+++ src/main/java/online/umbcraft/web/SetKeyScraper.java
@@ -6,11 +6,11 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
-public class PlaintextKeyScraper extends KeyScraper {
+public class SetKeyScraper extends KeyScraper {
 
     @Override
     public Future<Boolean> containsKey(final String targetURL, final String key) {
@@ -28,13 +28,8 @@ public class PlaintextKeyScraper extends KeyScraper {
                         throw new MalformedURLException("Invalid Document at URL: " + targetURL);
                     }
                     boolean matched = false;
-                    List<String> allTokens = reader.lines().collect(Collectors.toList());
-                    for(String token: allTokens) {
-                        if(key.equals(token)) {
-                            matched = true;
-                            break;
-                        }
-                    }
+                    Set<String> allTokens = reader.lines().collect(Collectors.toSet());
+                    matched = allTokens.contains(key);
                     reader.close();
                     isr.close();
                     is.close();
---
+++ src/main/java/online/umbcraft/web/StreamedKeyScraper.java
@@ -0,0 +1,46 @@
+package online.umbcraft.web;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+public class StreamedKeyScraper extends KeyScraper {
+
+    @Override
+    public Future<Boolean> containsKey(final String targetURL, final String key) {
+
+        return executor.submit(
+                () -> {
+                    URL url = new URL(targetURL);
+                    URLConnection conn = url.openConnection();
+                    InputStream is = conn.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(isr);
+
+                    String line = reader.readLine();
+                    if(!line.equals("----------BEGIN KEYS----------")) {
+                        throw new MalformedURLException("Invalid Document at URL: " + targetURL);
+                    }
+                    boolean matched = reader.lines().anyMatch(i -> i.equals(key));
+                    reader.close();
+                    isr.close();
+                    is.close();
+
+                    return matched;
+                }
+        );
+    }
+
+    @Override
+    public boolean validURL(String potentialURL) {
+
+        return true;
+
+    }
+}
