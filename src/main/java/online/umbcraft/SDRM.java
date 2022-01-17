107f1168a17cf7b91681ba957b45ae5357d96b84
Ivan8or <ivanthegreat825@gmail.com>
2022-01-17 03:12:42 -0500

    moved performance measuring code to its own class

--- src/main/java/online/umbcraft/SDRM.java
+++ src/main/java/online/umbcraft/SDRM.java
@@ -1,72 +1,15 @@
 package online.umbcraft;
 
 import online.umbcraft.hash.HashHelper;
-import online.umbcraft.web.KeyScraper;
-import online.umbcraft.web.SortedKeyScraper;
+import online.umbcraft.web.*;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class SDRM {
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-
-        File unsorted = new File("C:\\Users\\ivant\\IdeaProjects\\ServerlessDRM\\src\\main\\resources\\demo-keys.txt");
-        FileWriter unsortedw = new FileWriter(unsorted);
-
-        File sorted = new File("C:\\Users\\ivant\\IdeaProjects\\ServerlessDRM\\src\\main\\resources\\demo-keys-sorted.txt");
-        FileWriter sortedw = new FileWriter(sorted);
-        unsortedw.write("----------BEGIN KEYS----------");
-        sortedw.write("----------BEGIN KEYS----------");
-
-        List<String> hashes = new ArrayList<>(1000);
-        for(int i = 0; i < 1000; i++) {
-            String toDigest = i+"";
-            String hash = HashHelper.getHash(toDigest);
-            hashes.add(hash);
-            unsortedw.write("\n"+hash);
-        }
-        Collections.sort(hashes);
-        for(String s: hashes) {
-            sortedw.write("\n"+s);
-        }
-        sortedw.close();
-        unsortedw.close();
-
-        System.exit(0);
-
-        KeyScraper pks1 = new SortedKeyScraper();
-        String keyhash = HashHelper.getHash("449999");
-
-        String url = "https://raw.githubusercontent.com/Ivan8or/ServerlessDRM/master/src/main/resources/demo-keys-sorted.txt";
-
-        int trials = 50;
-
-        long[] v1times = new long[trials];
-
-        for(int i = 0; i < trials; i++) {
-            long milis = System.currentTimeMillis();
-            Future<Boolean> exists = pks1.containsKey(url, keyhash);
-            exists.get();
-            long milisf = System.currentTimeMillis();
-            System.out.println("v1 milis: " + (milisf - milis));
-            v1times[i] = (milisf - milis);
-        }
-        System.out.println("\n\n");
-
-        long average1 = Arrays.stream(v1times).sum() / trials;
-        System.out.println("v1: "+Arrays.toString(v1times));
-        System.out.println("v1 average: "+average1);
-
-        KeyScraper.getExecutorService().shutdown();
-    }
 
 
 }
---
+++ src/test/java/performance/PerformanceMeasures.java
@@ -0,0 +1,85 @@
+package performance;
+
+import online.umbcraft.hash.HashHelper;
+import online.umbcraft.web.GreedyKeyScraper;
+import online.umbcraft.web.KeyScraper;
+import online.umbcraft.web.StreamedKeyScraper;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class PerformanceMeasures {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+
+        KeyScraper v3scraper = new GreedyKeyScraper();
+        KeyScraper v4scraper = new StreamedKeyScraper();
+
+
+        String[] sizes = {"1k", "10k", "100k", "500k", "1000k"};
+        int[] trialsAmount = {200, 200, 50, 20, 20};
+
+        // very last token
+        //int[] tokenVal = {999, 9999, 99999, 499999, 999999};
+
+        // middle token
+        //int[] tokenVal = {499, 4999, 49999, 249999, 499999};
+
+        // first token
+        int[] tokenVal = {0, 0, 0, 0, 0};
+
+        String baseUrl = "https://raw.githubusercontent.com/Ivan8or/ServerlessDRM/master/demos/demo-";
+        //String baseUrl = "https://www.umbcraft.online/download/demo-";
+
+        //warmup
+        System.out.println("warming up");
+        trials(v3scraper, baseUrl + "100k-sorted.txt", 5000, 3);
+
+
+        for(int size = 0; size < sizes.length; size++) {
+            System.out.println("\n\n");
+            int trials = trialsAmount[size];
+            String sortedURL = baseUrl + sizes[size] + "-sorted.txt";
+            String unsortedURL = baseUrl + sizes[size] + ".txt";
+
+            long[] v3times = trials(v3scraper, unsortedURL, tokenVal[size], trials);
+            long[] v4times = trials(v4scraper, unsortedURL, tokenVal[size], trials);
+
+            System.out.println("Size: "+sizes[size]);
+            stats(v3times, "greedy");
+            stats(v4times, "streamed");
+        }
+
+
+        KeyScraper.getExecutorService().shutdown();
+    }
+
+    private static void stats(long[] trials, String label) {
+        long average1 = Arrays.stream(trials).sum() / trials.length;
+        //System.out.println(label+": " + Arrays.toString(trials));
+        System.out.println(label+" average: " + average1+"ms");
+    }
+
+    private static long[] trials(KeyScraper scraper, String url, int index, int num)
+            throws MalformedURLException, ExecutionException, InterruptedException {
+
+        String keyhash = HashHelper.getHash(index+"");
+
+        long[] times = new long[num];
+
+        for (int i = 0; i < num; i++) {
+            long milis = System.currentTimeMillis();
+            Future<Boolean> exists = scraper.containsKey(url, keyhash);
+            exists.get();
+            long milisf = System.currentTimeMillis();
+            System.out.print(".");
+            times[i] = (milisf - milis);
+        }
+        System.out.println();
+        return times;
+    }
+
+}
